3e84df1697f4484e00fde6788446594199a20ac1
drcoder <shakil.7878@gmail.com>
2021-01-23 15:40:16 +0530

    Alhamdulillah All portfolio site is completed and its update push from desktop

--- content/js/custom_scripts.js
+++ content/js/custom_scripts.js
@@ -5,7 +5,45 @@ $(document).ready(function () {
         } else {
             $('.navbar').removeClass("stickey");
         }
+        if (this.scrollY > 400) {
+            $('.scroll-up-button').addClass("show");
+        }
+        else {
+            $('.scroll-up-button').removeClass("show");
+        }
     });
+
+    // $(window).scrollbtn(function () {
+    //     if (this.scrollY > 500) {
+    //         $('.scroll-up-button').addClass("show");
+    //     }
+    //     else {
+    //         $('.scroll-up-button').removeClass("show");
+    //     }
+    // });
+
+    //slideup script
+    $('.scroll-up-button').click(function () {
+        $('html').animate({ scrollTop: 0 });
+    });
+
+    //typing animation script
+    var typed = new Typed('.typing', {
+        strings: ["Ui Developer", "Front-end Developer", "UI/Ux Designer", "Graphics Designer", "reactJS Ninja"],
+        typeSpeed: 100,
+        backSpeed: 40,
+        loop: true
+    });
+
+
+    //typing animation script
+    var typed = new Typed('.typing_2', {
+        strings: ["Ui Developer", "Front-end Developer", "UI/Ux Designer", "Graphics Designer", "reactJS Ninja"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true
+    });
+
 
     // toggle menu 
     $('.menu-btn').click(function () {
@@ -15,28 +53,28 @@ $(document).ready(function () {
 
 
     // owl script
-$('.carousel').owlCarousel({
-margin:20,
-loop: true,
-autoPlay:true,
-autoplayTimeOut:2000,
-autoplayHoverPause: true,
-responsive:{
-    0:{
-        items:1,
-        nav:false
-    },
-    600:{
-        items:2,
-        nav:false
-    },
-    1000: {
-        items:3,
-        nav:false
-    }
-}
-});
-    
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoPlay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
 });
 
 
